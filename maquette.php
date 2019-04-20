35160f21176aff34be6b7477979ec6cbc6bbe191
Pascal Nguyen <pasnguye@student.42.fr>
2019-04-20 18:09:36 +0200

    form

--- maquette.php
+++ maquette.php
@@ -21,8 +21,9 @@
 	
 		<section>
 			<div class='section'>
-				<div class='form'>
+				<div class='form-position'>
 					<form method="post" action="">
+						<div class='form'>
 							<span class='civility'>Civilité*</span>
    							<input type="radio" name="gender" value="Mme" id="Mme"/><label class='info'for="Mme">Mme</label>
    							<input type="radio" name="gender" value="Mr" id="Mr"/><label class='info' for="Mr">Mr</label><br />
@@ -32,6 +33,33 @@
    							<input type="text" name="ville" placeholder="Ville*"/><br />
    							<input type="text" name="social" placeholder="Raison sociale*"/><br />
    							<input type="text" name="tel" placeholder="Téléphone*"/><br />
+   						</div>
+   						<div class='submit'>
+   							<div class='ask'>Votre demande concerne :</div>
+   							<div class='btn-radio'>
+   								<div class='btn-radio-pos'>
+   									<input class='cd' type="radio" name="ask" value="Chèque Déjeuner" id="ChequeDej"/>
+   									<label class='radio_png' for="ChequeDej"><img src='img/chequeDejeuner.png' alt='chequeDejeuner'></label>
+   								</div>
+   								<div class='btn-radio-pos'>
+   									<input class='cd' type="radio" name="ask" value="Cadhoc" id="Cadhoc"/>
+   									<label class='radio_png' for="Cadhoc"><img src='img/Cadhoc.png' alt='Cadhoc'></label>
+   								</div>
+   								<div class='btn-radio-pos'>
+   									<input class='cd' type="radio" name="ask" value="Chèque Domicile" id="ChequeDom"/>
+   									<label class='radio_png' for="ChequeDom"><img src='img/ChequeDomicile.png' alt='ChequeDomicile'></label>
+   								</div>
+   							</div>
+   							<div class='btn-submit'>
+   								<div class='arrow'>
+   									<div class='arrow-position'><img src='img/arrow.png' alt='arrow'></div>
+   									
+   								</div>
+   								<div class='contact'>
+   									<div>ÊTRE RECONTACTÉ</div>
+   								</div>
+   							</div>
+   						</div>
 					</form>
 				</div>
 				<div>
