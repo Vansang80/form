3aa9e9a06becba54b99607f8df3b002f73c65056
Pascal Nguyen <pasnguye@student.42.fr>
2019-04-20 03:23:14 +0200

    form

---
+++ maquette.php
@@ -0,0 +1,43 @@
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel='stylesheet' href='style.css'>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:600" rel="stylesheet">
+		<title>Up</title>
+	</head>
+	<body>
+		<header>
+			<div class='header'>
+				<div class='header-logo'><img src='img/Logo-UP.png' alt='Up'></div>
+				<div class='header-title'>
+					<div>Titre</div>
+				</div>
+			</div>
+		</header>
+	
+		<section>
+			<div class='section'>
+				<div class='form'>
+					<form method="post" action="">
+							<span class='civility'>Civilité*</span>
+   							<input type="radio" name="gender" value="Mme" id="Mme"/><label class='info'for="Mme">Mme</label>
+   							<input type="radio" name="gender" value="Mr" id="Mr"/><label class='info' for="Mr">Mr</label><br />
+   							<input class='position-name' type="text" name="prenom" placeholder="Prénom*"/><br />
+   							<input type="text" name="nom" placeholder="Nom*"/><br />
+   							<input type="text" name="prenom" placeholder="Prénom*"/><br />
+   							<input type="text" name="ville" placeholder="Ville*"/><br />
+   							<input type="text" name="social" placeholder="Raison social*"/><br />
+   							<input type="text" name="tel" placeholder="Téléphone*"/><br />
+					</form>
+				</div>
+				<div>
+
+				</div>
+			<div>
+		</section>
+	</body>
+</html>
